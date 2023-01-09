82904e378f6ddf1e04eade9e7691200f720acdc7
tshino <tshino.ssr@gmail.com>
2023-01-10 01:54:38 +0900

    Call getApi only once

--- src/extension.js
+++ src/extension.js
@@ -2,31 +2,36 @@ const vscode = require('vscode');
 const kbmacro = require('./kbmacro.js');
 const dmacro = require('./dmacro.js');
 
-const repeat = async function() {
-    kbmacro.getApi().then(async api => {
-        const records = api.getRecentBackgroundRecords();
-        console.log(records);
+const repeat = async function(api) {
+    const records = api.getRecentBackgroundRecords();
+    console.log(records);
 
-        const sequenceToExecute = dmacro.detect(records, api.areEqualRecords);
-        if (0 < sequenceToExecute.length) {
-            await vscode.commands.executeCommand(
-                'kb-macro.playback',
-                { sequence: sequenceToExecute }
-            );
-        }
-    });
+    const sequenceToExecute = dmacro.detect(records, api.areEqualRecords);
+    if (0 < sequenceToExecute.length) {
+        await vscode.commands.executeCommand(
+            'kb-macro.playback',
+            { sequence: sequenceToExecute }
+        );
+    }
 };
 
 function activate(context) {
+    let api = null;
+
     context.subscriptions.push(
         vscode.commands.registerCommand(
             'dynamic-macro.repeat',
-            repeat
+            async function() {
+                if (api) {
+                    await repeat(api);
+                }
+            }
         )
     );
 
-    kbmacro.getApi().then(async api => {
-        await api.startBackgroundRecording();
+    kbmacro.getApi().then(async kbmacroApi => {
+        await kbmacroApi.startBackgroundRecording();
+        api = kbmacroApi;
     });
 }
 
